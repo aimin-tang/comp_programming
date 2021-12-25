133b879bea418f07691c4e46fed15d635a56c7c0
Aimin Tang <atang@Aimins-MacBook-Pro.local>
2021-12-25 09:36:37 -0600

    ch1_note

---
+++ ch1_notes.py
@@ -0,0 +1,40 @@
+# list indexes
+# l[i]
+# l[i:j]
+# l[:j]
+# l[i:]
+# l[-3:]
+# l[i:j:k]
+# l[::2]
+# l[::-1]
+
+# list functions
+# len(l) 1
+# sorted(l) nlogn
+# l.sort(l) nlogn
+# l.count(c) n
+# c in l n
+# l.append(c) amortized 1
+# l.pop(c) amortized 1
+
+l = [0 for _ in range(5)]
+for idx, v in enumerate(l):
+    print(f'{idx}: {v}')
+
+s = 'cowboy bebop'
+c = {letter: 0 for letter in s}
+
+# range
+# range(k, n)
+# range(k, n, 2)
+# range(n-1, -1, -1)
+
+l = [i for i in range(5)]
+def all_pairs(l):
+    n = len(l)
+    for i in range(n-1):
+        for j in range(i+1, n):
+            yield (l[i], l[j])
+
+for x, y in all_pairs(l):
+    print(x, y)
