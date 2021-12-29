5dab5cd70d6bce9d635b85cbbf9954f636297e0e
Aimin Tang <atang@Aimins-MacBook-Pro.local>
2021-12-29 10:40:28 -0600

    apple

---
+++ usaco/misc/cses_1623_apple.py
@@ -0,0 +1,36 @@
+import itertools
+import math
+import random
+
+
+def parse_input(input_str):
+    lines = input_str.strip().split('\n')
+
+    N = int(lines[0].strip())
+    apples = list(map(int, lines[1].strip().split()))
+
+    return N, apples
+
+def solve():
+    result = []
+    for i in range(1, N // 2 + 1):
+        for set1 in itertools.combinations(apples, i):
+            set2 = set(apples).difference(set1)
+            result.append((abs(sum(set1) - sum(set2)), set1, set2))
+
+    print(min(result)[0])
+
+if __name__ == '__main__':
+    input_str = """
+    5
+    3 2 7 4 1
+    """
+
+    N, apples = parse_input(input_str)
+    solve()
+
+
+    # testing
+    # print(N, apples)
+    # print('============')
+
