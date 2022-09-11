c793e613280821e2fba0146d71ff89f5dc9e1dc4
Aimin Tang <aimin.tang@gmail.com>
2022-09-11 17:18:15 -0500

    before blur

--- 2022/sept/img/lab.py
+++ 2022/sept/img/lab.py
@@ -1,15 +1,21 @@
 from images import *
 
 class Kernel:
-    def __init__(self, height, width, weights):
-        self.height = height
-        self.width = width
+    def __init__(self, weights):
+        """
+        for example, weights can be: [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+        """
+        self.height = len(weights)
+        self.width = len(weights[0])
         self.weights = weights
 
 class Image:
-    def __init__(self, height, width, pixels):
-        self.height = height
-        self.width = width
+    def __init__(self, pixels):
+        """
+        for example, pixels can be: [[1, 2], [3, 4], [5, 6]]
+        """
+        self.height = len(pixels)
+        self.width = len(pixels[0])
         self.pixels = pixels
 
     def __repr__(self):
@@ -18,35 +24,76 @@ class Image:
 
     def round_and_clip_image(self):
         new_pixels = [] 
-        for pixel in self.pixels:
-            if pixel > 255:
-                new_pixels.append(255)
-            else:
-                new_pixels.append(int(pixel))
 
-        return Image(self.height, self.width, new_pixels)
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                if self.pixels[i][j] > 255:
+                    row.append(255)
+                else:
+                    row.append(int(self.pixels[i][j]))
+            new_pixels.append(row)
+
+        return Image(new_pixels)
 
     def invert(self):
-        new_pixels = [255 - pixel for pixel in self.pixels]
+        new_pixels = [] 
 
-        new_image = Image(self.height, self.width, new_pixels)
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                row.append(255 - self.pixels[i][j])
+
+            new_pixels.append(row)
+
+        new_image = Image(new_pixels)
         return new_image.round_and_clip_image()
 
-    def get_pixel(self, p_height, p_width, k_height, k_width, mode):
-        return 0
+    def get_pixel(self, i, j, mode):
+        if 0 <= i < self.height and 0 <= j < self.width:
+            return self.pixels[i][j]
+        else:
+            if mode == 'zero':
+                return 0
+            elif mode == 'wrap':
+                if i < 0:
+                    i += self.height
+                if i >= self.height:
+                    i -= self.height
+                if j < 0:
+                    j += self.width
+                if j >= self.width:
+                    j -= self.width
+                return self.pixels[i][j]
+            elif mode == 'extend':
+                if i < 0:
+                    i = 0
+                if i >= self.height:
+                    i = self.height - 1
+                if j < 0:
+                    j = 0
+                if j >= self.width:
+                    j = self.width - 1
+                return self.pixels[i][j]
+            else:
+                raise RuntimeError(f'Unknown mode {mode}')
 
     def get_matrix(self, p_height, p_width, mode, kernel):
         matrix = []
+        adj_height = p_height - (kernel.height - 1) // 2
+        adj_width = p_width - (kernel.width - 1) // 2
 
         for k_height in range(kernel.height):
             row = []
             for k_width in range(kernel.width):
-                row.append(self.get_pixel(p_height, p_width, k_height, k_width, mode))
+                i = adj_height + k_height
+                j = adj_width + k_width
+                row.append(self.get_pixel(i, j, mode))
             matrix.append(row)
 
         return matrix
 
-    def filter_pixel(self, matrix, kernel):
+    def apply_kernel(self, matrix, kernel):
         pixel = 0
 
         for i in range(kernel.height):
@@ -59,17 +106,21 @@ class Image:
         new_pixels = []
 
         for height in range(self.height):
+            row = []
             for width in range(self.width):
                 matrix = self.get_matrix(height, width, mode, kernel)
-                pixel = self.filter_pixel(matrix, kernel)
-                new_pixels.append(pixel)
+                pixel = self.apply_kernel(matrix, kernel)
+                row.append(pixel)
+            new_pixels.append(row)
 
-        new_image = Image(self.height, self.width, new_pixels)
+        new_image = Image(new_pixels)
         return new_image.round_and_clip_image()
 
 if __name__ == '__main__':
-    image1 = Image(i1_data['height'], i1_data['width'], i1_data['pixels'])
+    image1 = Image(i1_data)
     print(image1)
     print(image1.invert())
-    k_identity = Kernel(3, 3, [[0, 0, 0], [0, 1, 0], [0, 0, 0]])
-    print(image1.filter('zero', k_identity))
+    k_identity = Kernel([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    print('zero:\n', image1.filter('zero', k_identity))
+    print('extend:\n', image1.filter('extend', k_identity))
+    print('wrap:\n', image1.filter('wrap', k_identity))
--- 2022/sept/img/test_lab.py
+++ 2022/sept/img/test_lab.py
@@ -1,9 +1,96 @@
-from images import *
-from lab import Image
+from lab import Image, Kernel
+
+def test_round_and_clip_image():
+    data = [[0, 50.1], [50, 100], [100, 258]]
+    image1 = Image(data)
+    new_image = image1.round_and_clip_image()
+    assert new_image.height == 3
+    assert new_image.width == 2
+    assert new_image.pixels == [[0, 50], [50, 100], [100, 255]]
 
 def test_invert():
-    image1 = Image(i1_data['height'], i1_data['width'], i1_data['pixels'])
+    data = [[0, 50], [50, 100], [100, 255]]
+    image1 = Image(data)
     new_image = image1.invert()
     assert new_image.height == 3
     assert new_image.width == 2
-    assert new_image.pixels == [255, 205, 205, 155, 155, 0]
+    assert new_image.pixels == [[255, 205], [205, 155], [155, 0]]
+
+def test_get_pixel_zero():
+    data = [[1, 50], [50, 100], [100, 255]]
+    image1 = Image(data)
+    pixel = image1.get_pixel(0, 0, 'zero')
+    assert pixel == 1
+    pixel = image1.get_pixel(-1, 0, 'zero')
+    assert pixel == 0
+    pixel = image1.get_pixel(2, -1, 'zero')
+    assert pixel == 0
+    pixel = image1.get_pixel(3, 0, 'zero')
+    assert pixel == 0
+    pixel = image1.get_pixel(2, 2, 'zero')
+    assert pixel == 0
+    pixel = image1.get_pixel(3, 3, 'zero')
+    assert pixel == 0
+
+def test_get_pixel_extend():
+    data = [[1, 2], [3, 4], [5, 6]]
+    image1 = Image(data)
+    pixel = image1.get_pixel(0, 0, 'extend')
+    assert pixel == 1
+    pixel = image1.get_pixel(-1, 0, 'extend')
+    assert pixel == 1
+    pixel = image1.get_pixel(2, -1, 'extend')
+    assert pixel == 5
+    pixel = image1.get_pixel(3, 0, 'extend')
+    assert pixel == 5
+    pixel = image1.get_pixel(2, 2, 'extend')
+    assert pixel == 6
+    pixel = image1.get_pixel(3, 3, 'extend')
+    assert pixel == 6
+
+def test_get_pixel_wrap():
+    data = [[1, 2], [3, 4], [5, 6]]
+    image1 = Image(data)
+    pixel = image1.get_pixel(0, 0, 'wrap')
+    assert pixel == 1
+    pixel = image1.get_pixel(-1, 0, 'wrap')
+    assert pixel == 5
+    pixel = image1.get_pixel(2, -1, 'wrap')
+    assert pixel == 6
+    pixel = image1.get_pixel(3, 0, 'wrap')
+    assert pixel == 1
+    pixel = image1.get_pixel(2, 2, 'wrap')
+    assert pixel == 5
+    pixel = image1.get_pixel(3, 3, 'wrap')
+    assert pixel == 2
+
+def test_get_matrix():
+    data = [[1, 2], [3, 4], [5, 6]]
+    image1 = Image(data)
+    k = Kernel([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    matrix = image1.get_matrix(0, 0, 'zero', k)
+    assert matrix == [[0, 0, 0], [0, 1, 2], [0, 3, 4]]
+
+def test_apply_kernel():
+    data = [[1, 2], [3, 4], [5, 6]]
+    image1 = Image(data)
+    matrix = [[0, 0, 0], [0, 1, 2], [0, 3, 4]]
+    k = Kernel([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    pixel = image1.apply_kernel(matrix, k)
+    assert pixel == 1
+
+def test_filter_zero():
+    data = [[1, 2], [3, 4], [5, 6]]
+    image1 = Image(data)
+    k = Kernel([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    new_image = image1.filter('zero', k)
+    assert new_image.pixels == [[1, 2], [3, 4], [5, 6]]
+
+def test_filter_extend():
+    data = [[1, 2], [3, 4], [5, 6]]
+    image1 = Image(data)
+    k = Kernel([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
+    new_image = image1.filter('extend', k)
+    assert new_image.pixels == [[1, 2], [3, 4], [5, 6]]
+
+
